0fe8398ca1d37668e853ef65e34a2760ed6eef9e
Mohamad-Jaafar NEHME <mohamad.jaafar.nehme@icloud.com>
2022-08-27 16:17:25 -0400

    #18 Enhance pdf

--- src/pdf.rs
+++ src/pdf.rs
@@ -1,21 +1,28 @@
-use crate::exam::Exam;
-use genpdf::Document;
+use crate::{
+    exam::{Exam, ExamType},
+    question_type::{self, QuestionType},
+};
+use genpdf::{style::Style, Document};
 
 use crate::question::Question;
 
 pub fn generate_pdf<T: Question>(exam: &Exam<T>) {
     println!("Generating PDF file...");
-    // Load a font from the file system
+
     let font_family = genpdf::fonts::from_files("./fonts", "LiberationSans", None)
         .expect("Failed to load font family");
-    // Create a document and set the default font family
     let mut doc = genpdf::Document::new(font_family);
-    // Change the default settings
     doc.set_title("Math Test");
-    // Customize the pages
     let mut decorator = genpdf::SimplePageDecorator::new();
     decorator.set_margins(10);
     doc.set_page_decorator(decorator);
+    if exam.get_question_type()
+        == question_type::QuestionType::Equation(question_type::EquationType::TwoVariables)
+    {
+        doc.set_font_size(11);
+    } else {
+        doc.set_font_size(12);
+    }
 
     let questions = exam.get();
     let mut counter = 1;
@@ -25,7 +32,6 @@ pub fn generate_pdf<T: Question>(exam: &Exam<T>) {
     }
 
     let file_name = "math_test.pdf";
-    // Render the document and write it to a file
     doc.render_to_file(file_name)
         .expect("Failed to write PDF file");
 
@@ -34,9 +40,16 @@ pub fn generate_pdf<T: Question>(exam: &Exam<T>) {
 
 fn push_question<T: Question>(question: &T, id: i32, doc: &mut Document) {
     let title = format!("Question {}: ", id);
-    doc.push(genpdf::elements::Paragraph::new(title));
 
-    doc.push(genpdf::elements::Paragraph::new(question.to_string()));
+    let mut title_element = genpdf::elements::Paragraph::new("");
+    let style = Style::new();
+    let style = style.bold();
+    title_element.push_styled(title, style);
+
+    doc.push(title_element);
+    for line in question.to_string().lines() {
+        doc.push(genpdf::elements::Paragraph::new(line));
+    }
 
     doc.push(genpdf::elements::Paragraph::new(""));
     let separator = "=============";
